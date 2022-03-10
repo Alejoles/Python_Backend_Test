530088b81d61f59365a940ecd8fddcd59e5d21c2
alejo <rulitosxd1@gmail.com>
2022-03-10 18:04:20 -0500

    almost finished, last review

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import timeit
 import sqlite3 as sql
 
 
-
 @alert_country
 def request_country():
     """
@@ -24,23 +23,22 @@ def encode_sha1(str):
 def get_info_country(json):
     """
         Gets information from the json obtained from the page
-        restcountries and aditional encrypts the language.
-        It returns the region, the name and the encrypted language.
-        Encrypts using encode_sha1(str)
+        restcountries.
+        It returns the region, the name and the language.
     """
     name = json['name']['common']
     region = json['region']
 
     if('languages' not in json):
         language = "No language"
-        return region, name, encode_sha1(language)
+        return region, name, language
 
     languages = json['languages']
 
     for i in languages:
         language = languages[i]
         break
-    return region, name, encode_sha1(language)
+    return region, name, language
 
 
 def time_values(df):
@@ -48,10 +46,15 @@ def time_values(df):
         Prints the information of the column Time of
         the DataFrame of pandas.
     """
-    print('Total time: ' + str(df['Time(ms)'].sum()) + ' ms')
-    print('Mean time: ' + str(df['Time(ms)'].mean()) + ' ms')
-    print('Min time: ' + str(df['Time(ms)'].min()) + ' ms')
-    print('Max time: ' + str(df['Time(ms)'].max()) + ' ms')
+    total = df['Time(ms)'].sum()
+    mean = df['Time(ms)'].mean()
+    min = df['Time(ms)'].min()
+    max = df['Time(ms)'].max()
+    print('Total time: ' + str(total) + ' ms')
+    print('Mean time: ' + str(mean) + ' ms')
+    print('Min time: ' + str(min) + ' ms')
+    print('Max time: ' + str(max) + ' ms')
+    return total, mean, min, max
 
 
 def sql_data_write(df):
@@ -79,20 +82,20 @@ def main():
     time = []
     stats = request_country()
     df = pd.DataFrame(columns=['Region', 'Country', 'Language'])
-    
+
     for i in stats:
         start = timeit.default_timer()
         info = get_info_country(i)
-        df.loc[len(df.index)] = info
+        df.loc[len(df.index)] = info[0], info[1], encode_sha1(info[2])
         stop = timeit.default_timer()
         time.append(float("{:.2f}".format((stop - start)*1000)))
     df['Time(ms)'] = time  # Adding the column time with its values
-    
+
     time_values(df)
 
     df.to_json(r'./data.json')  # Creates the .json in the actual directory
 
-    sql_data_write(df)
+    #  sql_data_write(df)
     sql_data_read(df)
 
 
--- tests.py
+++ tests.py
@@ -1,9 +1,26 @@
+import json
 import unittest
-from getcountries import alert_country
-from main import request_country
+import hashlib
+from main import request_country, get_info_country
 
+
+def sha1_jsonfile():
+    file = open('data.json')
+    filelanguage = json.loads(file.read())['Language']
+    file.close()
+    data = request_country()
+    languages = {}
+    count = 0
+    for i in data:
+        languages[str(count)] = hashlib.sha1(get_info_country(i)[2].encode()).hexdigest()
+        count += 1
+    return languages, filelanguage
 
 
 class TestCountry(unittest.TestCase):
-    def test_for_type_list(self):
+
+    def test_for_type_list_countries(self):
         self.assertEqual(type(request_country()), list)
+
+    def test_sha1_code(self):
+        self.assertDictEqual(sha1_jsonfile()[0], sha1_jsonfile()[1])
